a8a225b6558d0e41a867335152697f71f7c84107
HappyFee <32046763+HappyFee@users.noreply.github.com>
2019-08-22 14:10:59 +0800

    Update Alex_keras.py

--- AlexNet/utils/Alex_keras.py
+++ AlexNet/utils/Alex_keras.py
@@ -1,4 +1,5 @@
 def LeNet():
+    
     model = Sequential()
     model.add(Conv2D(32,(5,5),strides=(1,1),input_shape=(28,28,1),padding='valid',activation='relu',kernel_initializer='uniform'))
     model.add(MaxPooling2D(pool_size=(2,2)))
@@ -7,4 +8,23 @@ def LeNet():
     model.add(Flatten())
     model.add(Dense(100,activation='relu'))
     model.add(Dense(10,activation='softmax'))
+    return model
+
+def AlexNet():
+
+    model = Sequential()
+    model.add(Conv2D(96,(11,11),strides=(4,4),input_shape=(227,227,3),padding='valid',activation='relu',kernel_initializer='uniform'))
+    model.add(MaxPooling2D(pool_size=(3,3),strides=(2,2)))
+    model.add(Conv2D(256,(5,5),strides=(1,1),padding='same',activation='relu',kernel_initializer='uniform'))
+    model.add(MaxPooling2D(pool_size=(3,3),strides=(2,2)))
+    model.add(Conv2D(384,(3,3),strides=(1,1),padding='same',activation='relu',kernel_initializer='uniform'))
+    model.add(Conv2D(384,(3,3),strides=(1,1),padding='same',activation='relu',kernel_initializer='uniform'))
+    model.add(Conv2D(256,(3,3),strides=(1,1),padding='same',activation='relu',kernel_initializer='uniform'))
+    model.add(MaxPooling2D(pool_size=(3,3),strides=(2,2)))
+    model.add(Flatten())
+    model.add(Dense(4096,activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(4096,activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(1000,activation='softmax'))
     return model
