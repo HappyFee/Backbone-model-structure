ecc61b591620148a89c9ee8b6c26260b3f2ecac7
HappyFee <32046763+HappyFee@users.noreply.github.com>
2019-08-22 14:06:09 +0800

    Update Alex_keras.py

--- AlexNet/utils/Alex_keras.py
+++ AlexNet/utils/Alex_keras.py
@@ -1,1 +1,10 @@
-
+def LeNet():
+    model = Sequential()
+    model.add(Conv2D(32,(5,5),strides=(1,1),input_shape=(28,28,1),padding='valid',activation='relu',kernel_initializer='uniform'))
+    model.add(MaxPooling2D(pool_size=(2,2)))
+    model.add(Conv2D(64,(5,5),strides=(1,1),padding='valid',activation='relu',kernel_initializer='uniform'))
+    model.add(MaxPooling2D(pool_size=(2,2)))
+    model.add(Flatten())
+    model.add(Dense(100,activation='relu'))
+    model.add(Dense(10,activation='softmax'))
+    return model
